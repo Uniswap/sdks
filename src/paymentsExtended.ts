087a66ce98aae25c2bb30c798ee8f299772b2051
Noah Zinsmeister <noahwz@gmail.com>
2021-11-19 15:39:42 -0500

    first pass

--- src/paymentsExtended.ts
+++ src/paymentsExtended.ts
@@ -1,9 +1,12 @@
-import JSBI from 'jsbi'
 import { Interface } from '@ethersproject/abi'
-import { Payments, FeeOptions } from '@uniswap/v3-sdk'
-import { abi } from '@uniswap/swap-router-contracts/artifacts/contracts/interfaces/IPeripheryPaymentsWithFeeExtended.sol/IPeripheryPaymentsWithFeeExtended.json'
-import { toHex } from '@uniswap/v3-sdk'
 import { Percent, Token, validateAndParseAddress } from '@uniswap/sdk-core'
+import { abi } from '@uniswap/swap-router-contracts/artifacts/contracts/interfaces/IPeripheryPaymentsWithFeeExtended.sol/IPeripheryPaymentsWithFeeExtended.json'
+import { FeeOptions, Payments, toHex } from '@uniswap/v3-sdk'
+import JSBI from 'jsbi'
+
+function encodeFeeBips(fee: Percent): string {
+  return toHex(fee.multiply(10_000).quotient)
+}
 
 export abstract class PaymentsExtended {
   public static INTERFACE: Interface = new Interface(abi)
@@ -13,10 +16,6 @@ export abstract class PaymentsExtended {
    */
   private constructor() {}
 
-  private static encodeFeeBips(fee: Percent): string {
-    return toHex(fee.multiply(10_000).quotient)
-  }
-
   public static encodeUnwrapWETH9(amountMinimum: JSBI, recipient?: string, feeOptions?: FeeOptions): string {
     // if there's a recipient, just pass it along
     if (typeof recipient === 'string') {
@@ -24,13 +23,13 @@ export abstract class PaymentsExtended {
     }
 
     if (!!feeOptions) {
-      const feeBips = this.encodeFeeBips(feeOptions.fee)
+      const feeBips = encodeFeeBips(feeOptions.fee)
       const feeRecipient = validateAndParseAddress(feeOptions.recipient)
 
       return PaymentsExtended.INTERFACE.encodeFunctionData('unwrapWETH9WithFee(uint256,uint256,address)', [
         toHex(amountMinimum),
         feeBips,
-        feeRecipient
+        feeRecipient,
       ])
     } else {
       return PaymentsExtended.INTERFACE.encodeFunctionData('unwrapWETH9(uint256)', [toHex(amountMinimum)])
@@ -49,21 +48,28 @@ export abstract class PaymentsExtended {
     }
 
     if (!!feeOptions) {
-      const feeBips = this.encodeFeeBips(feeOptions.fee)
+      const feeBips = encodeFeeBips(feeOptions.fee)
       const feeRecipient = validateAndParseAddress(feeOptions.recipient)
 
       return PaymentsExtended.INTERFACE.encodeFunctionData('sweepTokenWithFee(address,uint256,uint256,address)', [
         token.address,
         toHex(amountMinimum),
         feeBips,
-        feeRecipient
+        feeRecipient,
       ])
     } else {
-      return PaymentsExtended.INTERFACE.encodeFunctionData('sweepToken(address,uint256)', [token.address, toHex(amountMinimum)])
+      return PaymentsExtended.INTERFACE.encodeFunctionData('sweepToken(address,uint256)', [
+        token.address,
+        toHex(amountMinimum),
+      ])
     }
   }
 
   public static encodePull(token: Token, amount: JSBI): string {
     return PaymentsExtended.INTERFACE.encodeFunctionData('pull', [token.address, toHex(amount)])
+  }
+
+  public static encodeWrapETH(amount: JSBI): string {
+    return PaymentsExtended.INTERFACE.encodeFunctionData('wrapETH', [toHex(amount)])
   }
 }
