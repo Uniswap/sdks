1ee1761c9f76e1ee460a3d8abed0546f0dd73959
Mark Toda <toda.mark@gmail.com>
2022-09-15 10:26:51 -0700

    fix: add anvil network

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,11 @@
 export const PERMIT_POST_MAPPING: { readonly [key: number]: string } = {
   1: '0x0000000000000000000000000000000000000000',
+  12341234: '0xFD6D23eE2b6b136E34572fc80cbCd33E9787705e',
 };
 
 export const ORDER_QUOTER_MAPPING: { readonly [key: number]: string } = {
   1: '0x0000000000000000000000000000000000000000',
+  12341234: '0x1D13fF25b10C9a6741DFdce229073bed652197c7',
 };
 
 export enum OrderType {
@@ -22,6 +24,9 @@ type ReverseReactorMapping = {
 export const REACTOR_ADDRESS_MAPPING: ReactorMapping = {
   1: {
     [OrderType.DutchLimit]: '0x0000000000000000000000000000000000000000',
+  },
+  12341234: {
+    [OrderType.DutchLimit]: '0x4DAf17c8142A483B2E2348f56ae0F2cFDAe22ceE',
   },
 };
 
