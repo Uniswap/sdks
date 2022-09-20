bcf200d91fd140863a2a4164b5caa2982dd410f8
Mark Toda <toda.mark@gmail.com>
2022-09-19 17:08:00 -0700

    feat: add quoting

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './PermitPost';
 export * from './OrderValidator';
 export * from './NonceManager';
+export * from './OrderQuoter';
 
 export function stripHexPrefix(a: string): string {
   if (a.startsWith('0x')) {
