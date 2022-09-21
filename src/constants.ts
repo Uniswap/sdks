81fe94b3ecb485cb5ec7f99eba905e25defaf760
Mark Toda <toda.mark@gmail.com>
2022-09-21 00:08:12 -0700

    feat: add constants for mainnet fork

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 export const PERMIT_POST_MAPPING: { readonly [key: number]: string } = {
-  1: "0x0000000000000000000000000000000000000000",
+  1: "0x33d86A12Fb907fC6f49eF0A04524648117825cda",
   12341234: "0xFD6D23eE2b6b136E34572fc80cbCd33E9787705e",
 };
 
 export const ORDER_QUOTER_MAPPING: { readonly [key: number]: string } = {
-  1: "0x0000000000000000000000000000000000000000",
+  1: "0x78FdE388B0FfB1D692e48370192583e5f5609CB7",
   12341234: "0x1D13fF25b10C9a6741DFdce229073bed652197c7",
 };
 
@@ -23,7 +23,7 @@ type ReverseReactorMapping = {
 
 export const REACTOR_ADDRESS_MAPPING: ReactorMapping = {
   1: {
-    [OrderType.DutchLimit]: "0x0000000000000000000000000000000000000000",
+    [OrderType.DutchLimit]: "0x4ee00c53aB86Ad44b405E6367F8be72333a7b7C9",
   },
   12341234: {
     [OrderType.DutchLimit]: "0x4DAf17c8142A483B2E2348f56ae0F2cFDAe22ceE",
