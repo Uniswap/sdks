5c71424ec226aee8dd0474d1da1ad6471905b842
Mark Toda <toda.mark@gmail.com>
2022-09-28 12:13:46 -0700

    feat: add goerli deployment

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,12 @@
 export const PERMIT_POST_MAPPING: { readonly [key: number]: string } = {
   1: "0x320b2ADcf134128D067693417Be59754F221D9DE",
+  5: "0x99E76e161AA20a00EcE81DCCD6398169353945de",
   12341234: "0xF5c9D4eFCAe2e36993C0815d3A8Dfd3E8985b677",
 };
 
 export const ORDER_QUOTER_MAPPING: { readonly [key: number]: string } = {
   1: "0x16731f587daE8414fE636fC12BC4b5100907cCA9",
+  5: "0x3946398638B21cE394841537EC1c1F557491971a",
   12341234: "0x0836D41eBFFFF6cD0849770f82c922Cf14C9De95",
 };
 
@@ -24,6 +26,9 @@ type ReverseReactorMapping = {
 export const REACTOR_ADDRESS_MAPPING: ReactorMapping = {
   1: {
     [OrderType.DutchLimit]: "0xE9781560d93c27aa4C4F3543631d191D10608d20",
+  },
+  5: {
+    [OrderType.DutchLimit]: "0xE5D50eB6e669C32D797379aF3907478FE491036D",
   },
   12341234: {
     [OrderType.DutchLimit]: "0xFF086b7696Dc4116B336Dd0e42ecd2164FC2712B",
