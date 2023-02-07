43d698a2b13f56a578f295fbd4b22d1d288addfc
Tina Zheng <tina.s.zheng+github@gmail.com>
2023-02-07 12:55:00 -0500

    add comment for native currency handling

--- src/trade/utils.ts
+++ src/trade/utils.ts
@@ -6,6 +6,9 @@ export function areCurrenciesEqual(
   chainId: number
 ) {
   if (currency.chainId !== chainId) return false;
+
+  // TODO: once native currencies are supported by dutch limit order trades, add handling based on
+  // shared native currency address format
   if (currency.isNative) {
     throw new Error(
       "native currencies are not currently supported by DutchLimitOrder trades"
