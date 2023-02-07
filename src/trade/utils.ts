1d01e8c2f5a1d5ef437d9785fa5ef4873107eadd
Tina Zheng <tina.s.zheng+github@gmail.com>
2023-02-07 12:47:28 -0500

    add trade type

---
+++ src/trade/utils.ts
@@ -0,0 +1,10 @@
+import { Currency } from "@uniswap/sdk-core"
+
+export function areCurrenciesEqual(currency: Currency, address: string | null, chainId: number) {
+    if (currency.chainId !== chainId) return false
+    if (currency.isNative) {
+        throw new Error("native currencies are not currently supported by DutchLimitOrder trades")
+    }
+
+    return currency.address.toLowerCase() === address?.toLowerCase()
+  }
