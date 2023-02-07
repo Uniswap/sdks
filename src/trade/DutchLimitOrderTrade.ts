333218a2453789a31ea92aa4f919b5c28c9d7a5b
Tina Zheng <tina.s.zheng+github@gmail.com>
2023-02-07 12:53:44 -0500

    run yarn fix

--- src/trade/DutchLimitOrderTrade.ts
+++ src/trade/DutchLimitOrderTrade.ts
@@ -1,142 +1,166 @@
-import { Currency, CurrencyAmount, Price, TradeType } from "@uniswap/sdk-core"
+import { Currency, CurrencyAmount, Price, TradeType } from "@uniswap/sdk-core";
 
-import { DutchLimitOrder, DutchLimitOrderInfo } from "../order"
+import { DutchLimitOrder, DutchLimitOrderInfo } from "../order";
 
-import { areCurrenciesEqual } from "./utils"
+import { areCurrenciesEqual } from "./utils";
 
-export class DutchLimitOrderTrade<TInput extends Currency, TOutput extends Currency, TTradeType extends TradeType> {  
-    public readonly tradeType: TTradeType
-    public readonly order: DutchLimitOrder
-  
-    private _inputAmount: CurrencyAmount<TInput> | undefined
-    private _outputAmounts: CurrencyAmount<TOutput>[] | undefined
-  
-    private _currencyIn: TInput
-    private _currenciesOut: TOutput[]
-  
-    public constructor({
-      currencyIn,
-      currenciesOut,
-      orderInfo,
-      tradeType,
-    }: {
-      currencyIn: TInput
-      currenciesOut: TOutput[]
-      orderInfo: DutchLimitOrderInfo
-      tradeType: TTradeType
-    }) {
-      this._currencyIn = currencyIn
-      this._currenciesOut = currenciesOut
-      this.tradeType = tradeType
-  
-      // assume single-chain for now
-      this.order = new DutchLimitOrder(orderInfo, currencyIn.chainId)
-    }
-  
-    public get inputAmount(): CurrencyAmount<TInput> {
-      if (this._inputAmount) return this._inputAmount
-  
-      const amount = CurrencyAmount.fromRawAmount(this._currencyIn, this.order.info.input.startAmount.toString())
-      this._inputAmount = amount
-      return amount
-    }
-  
-    public get outputAmounts(): CurrencyAmount<TOutput>[] {
-      if (this._outputAmounts) return this._outputAmounts
-  
-      const amounts = this.order.info.outputs.map((output) => {
-        // assume single chain ids across all outputs for now
-        const currencyOut = this._currenciesOut.find((currency) =>
-          areCurrenciesEqual(currency, output.token, currency.chainId)
-        )
-  
-        if (!currencyOut) {
-            throw new Error('currency not found in output array')
-        }
+export class DutchLimitOrderTrade<
+  TInput extends Currency,
+  TOutput extends Currency,
+  TTradeType extends TradeType
+> {
+  public readonly tradeType: TTradeType;
+  public readonly order: DutchLimitOrder;
 
-        return CurrencyAmount.fromRawAmount(currencyOut, output.startAmount.toString())
-      })
-  
-      this._outputAmounts = amounts
-      return amounts
-    }
-  
-    private _firstNonFeeOutputStartEndAmounts:
-      | {
-          startAmount: CurrencyAmount<TOutput>
-          endAmount: CurrencyAmount<TOutput>
-        }
-      | undefined
-  
-    private getFirstNonFeeOutputStartEndAmounts(): {
-      startAmount: CurrencyAmount<TOutput>
-      endAmount: CurrencyAmount<TOutput>
-    } {
-      if (this._firstNonFeeOutputStartEndAmounts) return this._firstNonFeeOutputStartEndAmounts
-  
-      const output = this.order.info.outputs.find((output) => !output.isFeeOutput)
-      if (!output) {
-        throw new Error('there must be at least one non-fee output token')
-      }
-  
+  private _inputAmount: CurrencyAmount<TInput> | undefined;
+  private _outputAmounts: CurrencyAmount<TOutput>[] | undefined;
+
+  private _currencyIn: TInput;
+  private _currenciesOut: TOutput[];
+
+  public constructor({
+    currencyIn,
+    currenciesOut,
+    orderInfo,
+    tradeType,
+  }: {
+    currencyIn: TInput;
+    currenciesOut: TOutput[];
+    orderInfo: DutchLimitOrderInfo;
+    tradeType: TTradeType;
+  }) {
+    this._currencyIn = currencyIn;
+    this._currenciesOut = currenciesOut;
+    this.tradeType = tradeType;
+
+    // assume single-chain for now
+    this.order = new DutchLimitOrder(orderInfo, currencyIn.chainId);
+  }
+
+  public get inputAmount(): CurrencyAmount<TInput> {
+    if (this._inputAmount) return this._inputAmount;
+
+    const amount = CurrencyAmount.fromRawAmount(
+      this._currencyIn,
+      this.order.info.input.startAmount.toString()
+    );
+    this._inputAmount = amount;
+    return amount;
+  }
+
+  public get outputAmounts(): CurrencyAmount<TOutput>[] {
+    if (this._outputAmounts) return this._outputAmounts;
+
+    const amounts = this.order.info.outputs.map((output) => {
       // assume single chain ids across all outputs for now
       const currencyOut = this._currenciesOut.find((currency) =>
         areCurrenciesEqual(currency, output.token, currency.chainId)
-      )
+      );
 
       if (!currencyOut) {
-        throw new Error('currency output from order must exist in currenciesOut list')
+        throw new Error("currency not found in output array");
       }
-  
-      const startEndAmounts = {
-        startAmount: CurrencyAmount.fromRawAmount(currencyOut, output.startAmount.toString()),
-        endAmount: CurrencyAmount.fromRawAmount(currencyOut, output.endAmount.toString()),
+
+      return CurrencyAmount.fromRawAmount(
+        currencyOut,
+        output.startAmount.toString()
+      );
+    });
+
+    this._outputAmounts = amounts;
+    return amounts;
+  }
+
+  private _firstNonFeeOutputStartEndAmounts:
+    | {
+        startAmount: CurrencyAmount<TOutput>;
+        endAmount: CurrencyAmount<TOutput>;
       }
-  
-      this._firstNonFeeOutputStartEndAmounts = startEndAmounts
-      return startEndAmounts
+    | undefined;
+
+  private getFirstNonFeeOutputStartEndAmounts(): {
+    startAmount: CurrencyAmount<TOutput>;
+    endAmount: CurrencyAmount<TOutput>;
+  } {
+    if (this._firstNonFeeOutputStartEndAmounts)
+      return this._firstNonFeeOutputStartEndAmounts;
+
+    const output = this.order.info.outputs.find(
+      (output) => !output.isFeeOutput
+    );
+    if (!output) {
+      throw new Error("there must be at least one non-fee output token");
     }
-  
-    // TODO: revise when there are actually multiple output amounts. for now, assume only one non-fee output at a time
-    public get outputAmount(): CurrencyAmount<TOutput> {
-      return this.getFirstNonFeeOutputStartEndAmounts().startAmount
+
+    // assume single chain ids across all outputs for now
+    const currencyOut = this._currenciesOut.find((currency) =>
+      areCurrenciesEqual(currency, output.token, currency.chainId)
+    );
+
+    if (!currencyOut) {
+      throw new Error(
+        "currency output from order must exist in currenciesOut list"
+      );
     }
-  
-    public minimumAmountOut(): CurrencyAmount<TOutput> {
-      return this.getFirstNonFeeOutputStartEndAmounts().endAmount
-    }
-  
-    public maximumAmountIn(): CurrencyAmount<TInput> {
-      return CurrencyAmount.fromRawAmount(this._currencyIn, this.order.info.input.endAmount.toString())
-    }
-  
-    private _executionPrice: Price<TInput, TOutput> | undefined
-  
-    /**
-     * The price expressed in terms of output amount/input amount.
-     */
-    public get executionPrice(): Price<TInput, TOutput> {
-      return (
-        this._executionPrice ??
-        (this._executionPrice = new Price(
-          this.inputAmount.currency,
-          this.outputAmount.currency,
-          this.inputAmount.quotient,
-          this.outputAmount.quotient
-        ))
-      )
-    }
-  
-    /**
-     * Return the execution price after accounting for slippage tolerance
-     * @returns The execution price
-     */
-    public worstExecutionPrice(): Price<TInput, TOutput> {
-      return new Price(
+
+    const startEndAmounts = {
+      startAmount: CurrencyAmount.fromRawAmount(
+        currencyOut,
+        output.startAmount.toString()
+      ),
+      endAmount: CurrencyAmount.fromRawAmount(
+        currencyOut,
+        output.endAmount.toString()
+      ),
+    };
+
+    this._firstNonFeeOutputStartEndAmounts = startEndAmounts;
+    return startEndAmounts;
+  }
+
+  // TODO: revise when there are actually multiple output amounts. for now, assume only one non-fee output at a time
+  public get outputAmount(): CurrencyAmount<TOutput> {
+    return this.getFirstNonFeeOutputStartEndAmounts().startAmount;
+  }
+
+  public minimumAmountOut(): CurrencyAmount<TOutput> {
+    return this.getFirstNonFeeOutputStartEndAmounts().endAmount;
+  }
+
+  public maximumAmountIn(): CurrencyAmount<TInput> {
+    return CurrencyAmount.fromRawAmount(
+      this._currencyIn,
+      this.order.info.input.endAmount.toString()
+    );
+  }
+
+  private _executionPrice: Price<TInput, TOutput> | undefined;
+
+  /**
+   * The price expressed in terms of output amount/input amount.
+   */
+  public get executionPrice(): Price<TInput, TOutput> {
+    return (
+      this._executionPrice ??
+      (this._executionPrice = new Price(
         this.inputAmount.currency,
         this.outputAmount.currency,
-        this.maximumAmountIn().quotient,
-        this.minimumAmountOut().quotient
-      )
-    }
+        this.inputAmount.quotient,
+        this.outputAmount.quotient
+      ))
+    );
   }
+
+  /**
+   * Return the execution price after accounting for slippage tolerance
+   * @returns The execution price
+   */
+  public worstExecutionPrice(): Price<TInput, TOutput> {
+    return new Price(
+      this.inputAmount.currency,
+      this.outputAmount.currency,
+      this.maximumAmountIn().quotient,
+      this.minimumAmountOut().quotient
+    );
+  }
+}
--- src/trade/utils.ts
+++ src/trade/utils.ts
@@ -1,10 +1,16 @@
-import { Currency } from "@uniswap/sdk-core"
+import { Currency } from "@uniswap/sdk-core";
 
-export function areCurrenciesEqual(currency: Currency, address: string | null, chainId: number) {
-    if (currency.chainId !== chainId) return false
-    if (currency.isNative) {
-        throw new Error("native currencies are not currently supported by DutchLimitOrder trades")
-    }
-
-    return currency.address.toLowerCase() === address?.toLowerCase()
+export function areCurrenciesEqual(
+  currency: Currency,
+  address: string | null,
+  chainId: number
+) {
+  if (currency.chainId !== chainId) return false;
+  if (currency.isNative) {
+    throw new Error(
+      "native currencies are not currently supported by DutchLimitOrder trades"
+    );
   }
+
+  return currency.address.toLowerCase() === address?.toLowerCase();
+}
