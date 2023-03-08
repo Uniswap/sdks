975b8ddc6b50a75ac0ebfcce060e56b215b80679
Mark Toda <toda.mark@gmail.com>
2023-03-07 16:05:05 -0800

    fix: expose contracts

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -3,6 +3,7 @@
 /* eslint-disable */
 export type { DirectTakerExecutor } from "./DirectTakerExecutor";
 export type { DutchLimitOrderReactor } from "./DutchLimitOrderReactor";
+export type { ExclusiveFillerValidation } from "./ExclusiveFillerValidation";
 export type { MockERC20 } from "./MockERC20";
 export type { OrderQuoter } from "./OrderQuoter";
 export type { Permit2 } from "./Permit2";
@@ -13,6 +14,7 @@ export * as factories from "./factories";
 export { DeploylessMulticall2__factory } from "./factories/DeploylessMulticall2__factory";
 export { DirectTakerExecutor__factory } from "./factories/DirectTakerExecutor__factory";
 export { DutchLimitOrderReactor__factory } from "./factories/DutchLimitOrderReactor__factory";
+export { ExclusiveFillerValidation__factory } from "./factories/ExclusiveFillerValidation__factory";
 export { MockERC20__factory } from "./factories/MockERC20__factory";
 export { Multicall2__factory } from "./factories/Multicall2__factory";
 export { OrderQuoter__factory } from "./factories/OrderQuoter__factory";
