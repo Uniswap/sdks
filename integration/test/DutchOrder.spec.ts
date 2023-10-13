0a59046deba80914706ddfb3b7b5a761b0c0007b
Mark Toda <toda.mark@gmail.com>
2023-10-13 12:43:34 -0700

    feat: update nonFeeRecipient
    Take feeRecipient and use it as exclusive recipient, update all other
    outputs with the new recipient

--- integration/test/DutchOrder.spec.ts
+++ integration/test/DutchOrder.spec.ts
@@ -123,8 +123,51 @@ describe('DutchOrder', () => {
       .true;
     expect(builtOutput.recipient).to.eq(swapperAddress);
 
-    order = preBuildOrder.nonFeeRecipient(ethers.constants.AddressZero).build();
+    order = preBuildOrder.nonFeeRecipient(ethers.constants.AddressZero, ethers.constants.AddressZero).build();
     expect(order.info.outputs[0].recipient).to.eq(ethers.constants.AddressZero);
+  });
+
+  it('nonFeeRecipient updates recipient for non fee outputs', async () => {
+    const amount = BigNumber.from(10).pow(18);
+    const deadline = await new BlockchainTime().secondsFromNow(1000);
+    const swapperAddress = await swapper.getAddress();
+    const feeRecipient = '0x1111111111111111111111111111111111111111';
+    const preBuildOrder = new DutchOrderBuilder(
+      chainId,
+      reactor.address,
+      permit2.address
+    )
+      .deadline(deadline)
+      .decayEndTime(deadline)
+      .decayStartTime(deadline - 100)
+      .swapper(swapperAddress)
+      .nonce(BigNumber.from(100))
+      .input({
+        token: tokenIn.address,
+        startAmount: amount,
+        endAmount: amount,
+      })
+      .output({
+        token: tokenOut.address,
+        startAmount: amount,
+        endAmount: BigNumber.from(10).pow(17).mul(9),
+        recipient: swapperAddress,
+      })
+      .output({
+        token: tokenOut.address,
+        startAmount: amount,
+        endAmount: BigNumber.from(10).pow(17).mul(9),
+        recipient: feeRecipient,
+      });
+
+    let order = preBuildOrder.build();
+
+    expect(order.info.outputs[0].recipient).to.eq(swapperAddress);
+    expect(order.info.outputs[1].recipient).to.eq(feeRecipient);
+
+    order = preBuildOrder.nonFeeRecipient(ethers.constants.AddressZero, feeRecipient).build();
+    expect(order.info.outputs[0].recipient).to.eq(ethers.constants.AddressZero);
+    expect(order.info.outputs[1].recipient).to.eq(feeRecipient);
   });
 
   it('executes a serialized order with no decay', async () => {
