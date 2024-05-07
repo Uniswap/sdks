65f75794ebb933ab23af1328cce336267fcc3c4a
Mark Toda <toda.mark@gmail.com>
2024-05-07 10:41:34 -0400

    fix: tests

--- sdks/uniswapx-sdk/src/constants.test.ts
+++ sdks/uniswapx-sdk/src/constants.test.ts
@@ -6,7 +6,7 @@ describe("REACTOR_ADDRESS_MAPPING", () => {
       Object {
         "1": Object {
           "Dutch": "0x6000da47483062A0D734Ba3dc7576Ce6A0B645C4",
-          "Dutch_V2": "0x3867393cC6EA7b0414C2c3e1D9fe7cEa987Fd066",
+          "Dutch_V2": "0x00000011F84B9aa48e5f8aA8B9897600006289Be",
           "Relay": "0x0000000000A4e21E2597DCac987455c48b12edBF",
         },
         "11155111": Object {
