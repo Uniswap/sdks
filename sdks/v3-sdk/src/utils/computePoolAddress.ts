762b679c73d9e6f05ba1f86caa71b882e747506a
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-06-06 14:01:42 -0700

    use sdk-core create2address func

--- sdks/v3-sdk/src/utils/computePoolAddress.ts
+++ sdks/v3-sdk/src/utils/computePoolAddress.ts
@@ -2,7 +2,7 @@ import { defaultAbiCoder } from '@ethersproject/abi'
 import { getCreate2Address } from '@ethersproject/address'
 import { keccak256 } from '@ethersproject/solidity'
 import { ChainId, Token } from '@uniswap/sdk-core'
-import { create2Address } from 'zksync-ethers/src/utils'
+import { computeZksyncCreate2Address } from '@uniswap/sdk-core'
 import { FeeAmount, poolInitCodeHash } from '../constants'
 
 /**
@@ -40,7 +40,7 @@ export function computePoolAddress({
   // ZKSync uses a different create2 address computation
   // Most likely all ZKEVM chains will use the different computation from standard create2
   if (chainId === ChainId.ZKSYNC) {
-    return create2Address(factoryAddress, initCodeHash, salt)
+    return computeZksyncCreate2Address(factoryAddress, initCodeHash, salt)
   } else {
     return getCreate2Address(factoryAddress, initCodeHash, salt)
   }
