759f08b711e97a3fed7535444265b2c83088dd59
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-06-05 17:17:21 -0700

    make chainid method param optional

--- sdks/permit2-sdk/src/constants.ts
+++ sdks/permit2-sdk/src/constants.ts
@@ -3,7 +3,7 @@ import { BigNumber } from '@ethersproject/bignumber'
 // @deprecated please use permit2Address(chainId: number)
 export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
 
-export function permit2Address(chainId: number): string {
+export function permit2Address(chainId?: number): string {
   switch (chainId) {
     case 324:
       return '0x0000000000225e31d15943971f47ad3022f714fa'
