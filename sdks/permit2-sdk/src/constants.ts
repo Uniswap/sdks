660d9d0dfa95446417d9c84eff29bc07a23b2bab
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-06-07 11:35:59 -0700

    feedback

--- sdks/permit2-sdk/src/constants.ts
+++ sdks/permit2-sdk/src/constants.ts
@@ -6,7 +6,7 @@ export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
 export function permit2Address(chainId?: number): string {
   switch (chainId) {
     case 324:
-      return '0x0000000000225e31d15943971f47ad3022f714fa'
+      return '0x0000000000225e31D15943971F47aD3022F714Fa'
     default:
       return PERMIT2_ADDRESS
   }
