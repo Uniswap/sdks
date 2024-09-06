1302e904e20678e159ce5d334f4c8f59f76f4d3f
dannydaniil <danny.daniil@uniswap.org>
2024-09-06 10:45:45 -0400

    introduce router versions

--- sdks/universal-router-sdk/src/index.ts
+++ sdks/universal-router-sdk/src/index.ts
@@ -3,10 +3,8 @@ export * from './entities'
 export * from './utils/routerTradeAdapter'
 export { RoutePlanner, CommandType } from './utils/routerCommands'
 export {
-  UNIVERSAL_ROUTER_ADDRESS_V1_2,
-  UNIVERSAL_ROUTER_CREATION_BLOCK_V1_2,
-  UNIVERSAL_ROUTER_ADDRESS_V2_0,
-  UNIVERSAL_ROUTER_CREATION_BLOCK_V2_0,
+  UNIVERSAL_ROUTER_CREATION_BLOCK,
+  UNIVERSAL_ROUTER_ADDRESS,
   PERMIT2_ADDRESS,
   ROUTER_AS_RECIPIENT,
   WETH_ADDRESS,
