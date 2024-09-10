3edd699a5e494ec8374e3f09fc681ece5256a7d1
jsy1218 <91580504+jsy1218@users.noreply.github.com>
2024-09-10 14:33:28 -0700

    feat: support mixed route with v4 route

--- sdks/router-sdk/src/utils/encodeMixedRouteToPath.ts
+++ sdks/router-sdk/src/utils/encodeMixedRouteToPath.ts
@@ -1,10 +1,17 @@
-import { pack } from '@ethersproject/solidity'
-import { Currency } from '@uniswap/sdk-core'
-import { Pool as V3Pool } from '@uniswap/v3-sdk'
-import { Pool as V4Pool } from '@uniswap/v4-sdk'
-import { MixedRouteSDK } from '../entities/mixedRoute/route'
-import { V2_FEE_PATH_PLACEHOLDER } from '../constants'
-import { TPool } from './TPool'
+import {pack} from '@ethersproject/solidity'
+import {Protocol} from "@uniswap/router-sdk/src/entities/protocol";
+import {Currency} from '@uniswap/sdk-core'
+import {Pair} from '@uniswap/v2-sdk'
+import {Pool as V3Pool} from '@uniswap/v3-sdk'
+import {Pool as V4Pool} from '@uniswap/v4-sdk'
+import {
+  MIXED_QUOTER_V2_V2_FEE_PATH_PLACEHOLDER,
+  MIXED_QUOTER_V2_V3_FEE_PATH_PLACEHOLDER,
+  MIXED_QUOTER_V2_V4_FEE_PATH_PLACEHOLDER,
+  V2_FEE_PATH_PLACEHOLDER
+} from '../constants'
+import {MixedRouteSDK} from '../entities/mixedRoute/route'
+import { TPool } from './TPool';
 
 /**
  * Converts a route to a hex encoded path
@@ -15,29 +22,53 @@ import { TPool } from './TPool'
 export function encodeMixedRouteToPath(route: MixedRouteSDK<Currency, Currency>): string {
   const firstInputToken: Currency = route.input.wrapped
 
+  const routeContainsV4Pool = (route.pools.filter((pool) => pool instanceof V4Pool).length > 0);
+
   const { path, types } = route.pools.reduce(
     (
       { inputToken, path, types }: { inputToken: Currency; path: (string | number)[]; types: string[] },
       pool: TPool,
       index
     ): { inputToken: Currency; path: (string | number)[]; types: string[] } => {
-      if (pool instanceof V4Pool) throw 'Encoding mixed routes with V4 not supported'
+      const poolVersion: Protocol = pool instanceof V4Pool ? Protocol.V4 : pool instanceof V3Pool ? Protocol.V3 : Protocol.V2
       const outputToken: Currency = pool.token0.equals(inputToken) ? pool.token1 : pool.token0
-      if (index === 0) {
-        return {
-          inputToken: outputToken,
-          types: ['address', 'uint24', 'address'],
-          path: [
-            inputToken.wrapped.address,
-            pool instanceof V3Pool ? pool.fee : V2_FEE_PATH_PLACEHOLDER,
-            outputToken.wrapped.address,
-          ],
+      const fee= derivePoolFee(pool)
+
+      if (routeContainsV4Pool) {
+        if (index === 0) {
+          return {
+            inputToken: outputToken,
+            types: poolVersionToInputTypes(index, ['address'], poolVersion),
+            path: [
+              inputToken.wrapped.address,
+              fee,
+              outputToken.wrapped.address,
+            ],
+          }
+        } else {
+          return {
+            inputToken: outputToken,
+            types: [...poolVersionToInputTypes(index, types, poolVersion)],
+            path: [...path, fee, outputToken.wrapped.address],
+          }
         }
       } else {
-        return {
-          inputToken: outputToken,
-          types: [...types, 'uint24', 'address'],
-          path: [...path, pool instanceof V3Pool ? pool.fee : V2_FEE_PATH_PLACEHOLDER, outputToken.wrapped.address],
+        if (index === 0) {
+          return {
+            inputToken: outputToken,
+            types: ['address', 'uint24', 'address'],
+            path: [
+              inputToken.wrapped.address,
+              pool instanceof V3Pool ? pool.fee : V2_FEE_PATH_PLACEHOLDER,
+              outputToken.wrapped.address,
+            ],
+          }
+        } else {
+          return {
+            inputToken: outputToken,
+            types: [...types, 'uint24', 'address'],
+            path: [...path, pool instanceof V3Pool ? pool.fee : V2_FEE_PATH_PLACEHOLDER, outputToken.wrapped.address],
+          }
         }
       }
     },
@@ -45,4 +76,33 @@ export function encodeMixedRouteToPath(route: MixedRouteSDK<Currency, Currency>)
   )
 
   return pack(types, path)
+}
+
+export function poolVersionToInputTypes(index: number, prependedTypes: string[], poolVersion: Protocol): string[] {
+  switch (poolVersion) {
+    case Protocol.V4:
+      if (index === 0) {
+          return [...prependedTypes, 'uint24', 'uint24', 'address', 'address']
+      } else {
+          return [...prependedTypes, 'uint24', 'address']
+      }
+    case Protocol.V3:
+      return [...prependedTypes, 'uint24', 'address']
+    case Protocol.V2:
+      return [...prependedTypes, 'uint8', 'address']
+    default:
+      throw new Error(`Unsupported pool version ${poolVersion}`)
+  }
+}
+
+export function derivePoolFee(pool: TPool): number {
+  if (pool instanceof Pair) {
+    return MIXED_QUOTER_V2_V2_FEE_PATH_PLACEHOLDER
+  } else if (pool instanceof V3Pool) {
+    return pool.fee + MIXED_QUOTER_V2_V3_FEE_PATH_PLACEHOLDER
+  } else if (pool instanceof V4Pool) {
+    return pool.fee + MIXED_QUOTER_V2_V4_FEE_PATH_PLACEHOLDER
+  } else {
+    throw new Error(`Unsupported pool type ${pool}`)
+  }
 }
