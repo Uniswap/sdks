81710abefdac9e718fc7daa2ef2e6c821455840d
Alan Wu <alanwu100@gmail.com>
2024-09-11 16:32:19 -0400

    style: linting

--- sdks/uniswapx-sdk/src/utils/dutchBlockDecay.ts
+++ sdks/uniswapx-sdk/src/utils/dutchBlockDecay.ts
@@ -1,120 +1,123 @@
 import { BigNumber } from "ethers";
 
-import { V3Decay } from "../order";
+import { NonlinearDutchDecay } from "../order";
 /*
 These functions mimic the smart contract functions as closely as possible to ensure that the same results are produced.
 Essentially Solidity translated to TypeScript.
 */
-/* eslint-disable */
 function locateArrayPosition(
-    curve: V3Decay,
-    targetValue: number
-  ): [number, number] {
-    const relativeBlocks = curve.relativeBlocks;
-    let prev = 0;
-    let next = 0;
-    while (next < curve.relativeAmounts.length) {
-      if (relativeBlocks[next] >= targetValue) {
-        return [prev, next];
-      }
-      prev = next;
-      next++;
-    }
-    return [next - 1, next - 1];
-  }
-  class NonLinearDutchDecayLib {
-    static decay(
-      curve: V3Decay,
-      startAmount: BigNumber,
-      decayStartBlock: number,
-      currentBlock: number
-    ): BigNumber {
-      // mismatch of relativeAmounts and relativeBlocks
-      if (curve.relativeAmounts.length > 16) {
-        throw new Error('InvalidDecayCurve');
-      }
-  
-      // handle current block before decay or no decay
-      if (decayStartBlock >= currentBlock || curve.relativeAmounts.length === 0) {
-        return startAmount;
-      }
-  
-      const blockDelta = currentBlock - decayStartBlock;
-  
-      // Special case for when we need to use the decayStartBlock (0)
-      if (curve.relativeBlocks[0] > blockDelta) {
-        return this.linearDecay(
-          0,
-          curve.relativeBlocks[0],
-          blockDelta,
-          startAmount,
-          startAmount.sub(curve.relativeAmounts[0])
-        );
-      }
-  
-      // the current pos is within or after the curve
-      let [prev, next] = locateArrayPosition(curve, blockDelta);
-      const lastAmount = startAmount.sub(curve.relativeAmounts[prev]);
-      const nextAmount = startAmount.sub(curve.relativeAmounts[next]);
-      return this.linearDecay(
-        curve.relativeBlocks[prev],
-        curve.relativeBlocks[next],
-        blockDelta,
-        lastAmount,
-        nextAmount
-      );
-    }
-  
-    static linearDecay(
-      startPoint: number,
-      endPoint: number,
-      currentPoint: number,
-      startAmount: BigNumber,
-      endAmount: BigNumber
-    ): BigNumber {
-      if (currentPoint >= endPoint) {
-        return endAmount;
-      }
-  
-      const elapsed = BigNumber.from(currentPoint - startPoint);
-      const duration = BigNumber.from(endPoint - startPoint);
-      if (endAmount.lt(startAmount)) {
-        return startAmount.sub(
-          startAmount.sub(endAmount.mul(elapsed).div(duration)) //muldivdown in contract
-        );
-      } else {
-        return startAmount.add(
-          endAmount.sub(startAmount.mul(elapsed).div(duration)) //muldivup in contract
-          //TODO: How can we do muldivup in JS?
-        );
-      }
-    }
-  }
-  
-  export { NonLinearDutchDecayLib };
+	curve: NonlinearDutchDecay,
+	targetValue: number
+): [number, number] {
+	const relativeBlocks = curve.relativeBlocks;
+	// eslint-disable-next-line prefer-const
+	let prev = 0;
+	// eslint-disable-next-line prefer-const
+	let next = 0;
+
+	while (next < curve.relativeAmounts.length) {
+		if (relativeBlocks[next] >= targetValue) {
+			return [prev, next];
+		}
+		prev = next;
+		next++;
+	}
+
+	return [next - 1, next - 1];
+}
+
+class NonLinearDutchDecayLib {
+	static decay(
+		curve: NonlinearDutchDecay,
+		startAmount: BigNumber,
+		decayStartBlock: number,
+		currentBlock: number
+	): BigNumber {
+		// mismatch of relativeAmounts and relativeBlocks
+		if (curve.relativeAmounts.length > 16) {
+			throw new Error('InvalidDecayCurve');
+		}
+
+		// handle current block before decay or no decay
+		if (decayStartBlock >= currentBlock || curve.relativeAmounts.length === 0) {
+			return startAmount;
+		}
+
+		const blockDelta = currentBlock - decayStartBlock;
+
+		// Special case for when we need to use the decayStartBlock (0)
+		if (curve.relativeBlocks[0] > blockDelta) {
+			return this.linearDecay(
+				0,
+				curve.relativeBlocks[0],
+				blockDelta,
+				startAmount,
+				startAmount.sub(curve.relativeAmounts[0])
+			);
+		}
+
+		// the current pos is within or after the curve
+		const [prev, next] = locateArrayPosition(curve, blockDelta);
+		const lastAmount = startAmount.sub(curve.relativeAmounts[prev]);
+		const nextAmount = startAmount.sub(curve.relativeAmounts[next]);
+		return this.linearDecay(
+			curve.relativeBlocks[prev],
+			curve.relativeBlocks[next],
+			blockDelta,
+			lastAmount,
+			nextAmount
+		);
+	}
+
+	static linearDecay(
+		startPoint: number,
+		endPoint: number,
+		currentPoint: number,
+		startAmount: BigNumber,
+		endAmount: BigNumber
+	): BigNumber {
+		if (currentPoint >= endPoint) {
+			return endAmount;
+		}
+
+		const elapsed = BigNumber.from(currentPoint - startPoint);
+		const duration = BigNumber.from(endPoint - startPoint);
+		if (endAmount.lt(startAmount)) {
+			return startAmount.sub(
+				startAmount.sub(endAmount.mul(elapsed).div(duration)) //muldivdown in contract
+			);
+		} else {
+			return startAmount.add(
+				endAmount.sub(startAmount.mul(elapsed).div(duration)) //muldivup in contract
+				//TODO: How can we do muldivup in JS?
+			);
+		}
+	}
+}
+
+export { NonLinearDutchDecayLib };
 
 export interface DutchBlockDecayConfig {
-    decayStartBlock: number;
-    startAmount: BigNumber;
-    relativeBlocks: number[];
-    relativeAmounts: BigNumber[];
+	decayStartBlock: number;
+	startAmount: BigNumber;
+	relativeBlocks: number[];
+	relativeAmounts: BigNumber[];
 }
 
 export function getBlockDecayedAmount(
-  config: DutchBlockDecayConfig,
-  atBlock: number
+	config: DutchBlockDecayConfig,
+	atBlock: number
 ): BigNumber {
-  const {decayStartBlock, startAmount, relativeBlocks, relativeAmounts} = config;
-  return NonLinearDutchDecayLib.decay({relativeAmounts, relativeBlocks}, startAmount, decayStartBlock, atBlock);
+	const { decayStartBlock, startAmount, relativeBlocks, relativeAmounts } = config;
+	return NonLinearDutchDecayLib.decay({ relativeAmounts, relativeBlocks }, startAmount, decayStartBlock, atBlock);
 }
 
 export function getEndAmount(
-    config: Partial<DutchBlockDecayConfig>
+	config: Partial<DutchBlockDecayConfig>
 ): BigNumber {
-    const { startAmount, relativeAmounts } = config;
-    if (!startAmount || !relativeAmounts) {
-        throw new Error("Invalid config for getting V3 decay end amount"); //TODO: Should we throw?
-    }
-    return startAmount.sub(relativeAmounts[relativeAmounts.length - 1]);
+	const { startAmount, relativeAmounts } = config;
+	if (!startAmount || !relativeAmounts) {
+		throw new Error("Invalid config for getting V3 decay end amount"); //TODO: Should we throw?
+	}
+	return startAmount.sub(relativeAmounts[relativeAmounts.length - 1]);
 }
-/* eslint-enable */
