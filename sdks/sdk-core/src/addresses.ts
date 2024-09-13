45d05b8e5d5c0ef944705ec45726049eb470fdf3
Siyu Jiang (See-You John) <91580504+jsy1218@users.noreply.github.com>
2024-09-13 09:38:12 -0700

    feat(sdk-core): add mixed quoter v2 property (#95)

--- sdks/sdk-core/src/addresses.ts
+++ sdks/sdk-core/src/addresses.ts
@@ -11,6 +11,7 @@ type ChainAddresses = {
   tickLensAddress?: string
   swapRouter02Address?: string
   mixedRouteQuoterV1Address?: string
+  mixedRouteQuoterV2Address?: string
 }
 
 const DEFAULT_NETWORKS = [ChainId.MAINNET, ChainId.GOERLI, ChainId.SEPOLIA]
@@ -162,6 +163,8 @@ const SEPOLIA_ADDRESSES: ChainAddresses = {
   nonfungiblePositionManagerAddress: '0x1238536071E1c677A632429e3655c799b22cDA52',
   tickLensAddress: '0xd7f33bcdb21b359c8ee6f0251d30e94832baad07',
   swapRouter02Address: '0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E',
+  // TODO: ROUTE-277 - update deploy address once after quoter refactoring.
+  mixedRouteQuoterV2Address: '0xa8b0be287acB850952DE4287b84B7222cc654C09',
 }
 
 // Avalanche v3 addresses
