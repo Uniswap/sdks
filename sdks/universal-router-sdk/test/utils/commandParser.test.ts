1aa16c3c8103139ed581e2846bd007b268e32cac
Mark Toda <toda.mark@gmail.com>
2024-10-01 11:16:54 -0400

    feat: more tests

--- sdks/universal-router-sdk/test/utils/commandParser.test.ts
+++ sdks/universal-router-sdk/test/utils/commandParser.test.ts
@@ -30,6 +30,110 @@ describe('Command Parser', () => {
         ],
       },
     },
+    {
+      input: new RoutePlanner().addCommand(CommandType.UNWRAP_WETH, [addressOne, amount]),
+      result: {
+        commands: [
+          {
+            commandName: 'UNWRAP_WETH',
+            commandType: CommandType.UNWRAP_WETH,
+            params: [
+              { name: 'recipient', value: addressOne },
+              { name: 'amountMin', value: amount },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      input: new RoutePlanner().addCommand(CommandType.SWEEP, [addressOne, addressTwo, amount]),
+      result: {
+        commands: [
+          {
+            commandName: 'SWEEP',
+            commandType: CommandType.SWEEP,
+            params: [
+              { name: 'token', value: addressOne },
+              { name: 'recipient', value: addressTwo },
+              { name: 'amountMin', value: amount },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      input: new RoutePlanner().addCommand(CommandType.TRANSFER, [addressOne, addressTwo, amount]),
+      result: {
+        commands: [
+          {
+            commandName: 'TRANSFER',
+            commandType: CommandType.TRANSFER,
+            params: [
+              { name: 'token', value: addressOne },
+              { name: 'recipient', value: addressTwo },
+              { name: 'value', value: amount },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      input: new RoutePlanner().addCommand(CommandType.PAY_PORTION, [addressOne, addressTwo, amount]),
+      result: {
+        commands: [
+          {
+            commandName: 'PAY_PORTION',
+            commandType: CommandType.PAY_PORTION,
+            params: [
+              { name: 'token', value: addressOne },
+              { name: 'recipient', value: addressTwo },
+              { name: 'bips', value: amount },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      input: new RoutePlanner().addCommand(CommandType.BALANCE_CHECK_ERC20, [addressOne, addressTwo, amount]),
+      result: {
+        commands: [
+          {
+            commandName: 'BALANCE_CHECK_ERC20',
+            commandType: CommandType.BALANCE_CHECK_ERC20,
+            params: [
+              { name: 'owner', value: addressOne },
+              { name: 'token', value: addressTwo },
+              { name: 'minBalance', value: amount },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      input: new RoutePlanner()
+        .addCommand(CommandType.WRAP_ETH, [addressOne, amount])
+        .addCommand(CommandType.UNWRAP_WETH, [addressOne, amount]),
+      result: {
+        commands: [
+          {
+            commandName: 'WRAP_ETH',
+            commandType: CommandType.WRAP_ETH,
+            params: [
+              { name: 'recipient', value: addressOne },
+              { name: 'amountMin', value: amount },
+            ],
+          },
+          {
+            commandName: 'UNWRAP_WETH',
+            commandType: CommandType.UNWRAP_WETH,
+            params: [
+              { name: 'recipient', value: addressOne },
+              { name: 'amountMin', value: amount },
+            ],
+          },
+        ],
+      },
+    },
   ]
 
   for (const test of tests) {
