45ed414f8a980eac8f166c7fcab41ca74e83577b
Alice <34962750+hensha256@users.noreply.github.com>
2024-10-10 13:51:49 -0400

    feat(v3-sdk): add decode to multicall (#164)

--- sdks/v3-sdk/src/multicall.test.ts
+++ sdks/v3-sdk/src/multicall.test.ts
@@ -22,4 +22,25 @@ describe('Multicall', () => {
       )
     })
   })
+
+  describe('#decodeMulticall', () => {
+    it('works for string array with length >1', async () => {
+      const calldatas: string[] = [
+        '0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        '0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+      ]
+
+      // first encode it
+      const multicall = Multicall.encodeMulticall(calldatas)
+      expect(multicall).toBe(
+        '0xac9650d800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000020aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0000000000000000000000000000000000000000000000000000000000000020bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+      )
+
+      // then decode it
+      const decodedCalldata = Multicall.decodeMulticall(multicall)
+      expect(decodedCalldata.length).toBe(calldatas.length)
+      expect(decodedCalldata[0]).toBe(calldatas[0])
+      expect(decodedCalldata[1]).toBe(calldatas[1])
+    })
+  })
 })
