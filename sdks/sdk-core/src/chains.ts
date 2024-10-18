40536a17ad474ee5d9e5fe739f85aff5c15c2737
pro100skm <pro100skm@yandex.ru>
2024-10-18 09:47:44 +0300

    feat(breaking): formatting

--- sdks/sdk-core/src/chains.ts
+++ sdks/sdk-core/src/chains.ts
@@ -1,16 +1,13 @@
 export enum ChainId {
   XDC = 50,
-  APOTHEM = 51
+  APOTHEM = 51,
 }
 
-export const SUPPORTED_CHAINS = [
-  ChainId.XDC,
-  ChainId.APOTHEM,
-] as const
-export type SupportedChainsType = typeof SUPPORTED_CHAINS[number]
+export const SUPPORTED_CHAINS = [ChainId.XDC, ChainId.APOTHEM] as const
+export type SupportedChainsType = (typeof SUPPORTED_CHAINS)[number]
 
 export enum NativeCurrencyName {
   // Strings match input for CLI
   XDC = 'XDC',
-  APOTHEM = 'TXDC'
+  APOTHEM = 'TXDC',
 }
--- sdks/sdk-core/src/entities/weth9.ts
+++ sdks/sdk-core/src/entities/weth9.ts
@@ -7,10 +7,10 @@ import { ChainId } from '../chains'
 export const WETH9: { [chainId in ChainId]: Token } = {
   [ChainId.XDC]: new Token(ChainId.XDC, '0x951857744785e80e2de051c32ee7b25f9c458c42', 18, 'WXDC', 'Wrapped XDC'),
   [ChainId.APOTHEM]: new Token(
-      ChainId.APOTHEM,
-      '0x2a5c77b016df1b3b0ae4e79a68f8adf64ee741ba',
-      18,
-      'WXDC',
-      'Wrapped XDC',
+    ChainId.APOTHEM,
+    '0x2a5c77b016df1b3b0ae4e79a68f8adf64ee741ba',
+    18,
+    'WXDC',
+    'Wrapped XDC'
   ),
 }
