4291ee93d8eb532c7e6598449f4eff6d13e71adc
Emily Williams <emag3m@gmail.com>
2024-10-24 14:34:43 -0400

    hooks utility class

---
+++ sdks/v4-sdk/src/hook.ts
@@ -0,0 +1,58 @@
+export type HookPermissions = {
+  beforeInitialize: boolean
+  afterInitialize: boolean
+  beforeAddLiquidity: boolean
+  afterAddLiquidity: boolean
+  beforeRemoveLiquidity: boolean
+  afterRemoveLiquidity: boolean
+  beforeSwap: boolean
+  afterSwap: boolean
+  beforeDonate: boolean
+  afterDonate: boolean
+  beforeSwapReturnsDelta: boolean
+  afterSwapReturnsDelta: boolean
+  afterAddLiquidityReturnsDelta: boolean
+  afterRemoveLiquidityReturnsDelta: boolean
+}
+
+export enum HookOptions {
+  AfterRemoveLiquidityReturnsDelta,
+  AfterAddLiquidityReturnsDelta,
+  AfterSwapReturnsDelta,
+  BeforeSwapReturnsDelta,
+  AfterDonate,
+  BeforeDonate,
+  AfterSwap,
+  BeforeSwap,
+  AfterRemoveLiquidity,
+  BeforeRemoveLiquidity,
+  AfterAddLiquidity,
+  BeforeAddLiquidity,
+  AfterInitialize,
+  BeforeInitialize,
+}
+
+export class Hook {
+  public static permissions(address: string): HookPermissions {
+    return {
+      beforeInitialize: this.hasPermission(address, HookOptions.BeforeInitialize),
+      afterInitialize: this.hasPermission(address, HookOptions.AfterInitialize),
+      beforeAddLiquidity: this.hasPermission(address, HookOptions.BeforeAddLiquidity),
+      afterAddLiquidity: this.hasPermission(address, HookOptions.AfterAddLiquidity),
+      beforeRemoveLiquidity: this.hasPermission(address, HookOptions.BeforeRemoveLiquidity),
+      afterRemoveLiquidity: this.hasPermission(address, HookOptions.AfterRemoveLiquidity),
+      beforeSwap: this.hasPermission(address, HookOptions.BeforeSwap),
+      afterSwap: this.hasPermission(address, HookOptions.AfterSwap),
+      beforeDonate: this.hasPermission(address, HookOptions.BeforeDonate),
+      afterDonate: this.hasPermission(address, HookOptions.AfterDonate),
+      beforeSwapReturnsDelta: this.hasPermission(address, HookOptions.BeforeSwapReturnsDelta),
+      afterSwapReturnsDelta: this.hasPermission(address, HookOptions.AfterSwapReturnsDelta),
+      afterAddLiquidityReturnsDelta: this.hasPermission(address, HookOptions.AfterAddLiquidityReturnsDelta),
+      afterRemoveLiquidityReturnsDelta: this.hasPermission(address, HookOptions.AfterRemoveLiquidityReturnsDelta),
+    }
+  }
+
+  public static hasPermission(address: string, permissionIndex: HookOptions) {
+    return !!(parseInt(address.slice(38), 16) & (1 << permissionIndex))
+  }
+}
