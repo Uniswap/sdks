3c83b94069b6169bd471e3b8b8739f5caa447c65
diana <diana.kocsis@uniswap.org>
2025-05-07 11:59:10 -0400

    fix(smart-wallet-sdk): mainnet, base, unichain deployments for v0.2.1-audit.2 (#362)
    Co-authored-by: Eric Zhong <eric.zhong@uniswap.org>

--- sdks/smart-wallet-sdk/src/constants.test.ts
+++ sdks/smart-wallet-sdk/src/constants.test.ts
@@ -2,13 +2,20 @@ import { getAllSmartWalletVersions, SMART_WALLET_ADDRESSES, SupportedChainIds, S
 
 describe('constants', () => {
   it('SMART_WALLET_ADDRESSES should be latest versions', () => {
-    // Since we only have one chain ID right now, test it directly
-    const chainId = SupportedChainIds.SEPOLIA
-    expect(SMART_WALLET_ADDRESSES[chainId]).toEqual(SMART_WALLET_VERSIONS[chainId][SmartWalletVersion.LATEST])
+    const chainIdValues = Object.values(SupportedChainIds)
+        .filter(value => typeof value === 'number');
+
+    for (const chainId of chainIdValues) {
+      expect(SMART_WALLET_ADDRESSES[chainId as SupportedChainIds]).toEqual(SMART_WALLET_VERSIONS[chainId as SupportedChainIds][SmartWalletVersion.LATEST])
+    }
   })
 
   it('getAllSmartWalletVersions should return all versions for a given chain id', () => {
-    const chainId = SupportedChainIds.SEPOLIA
-    expect(getAllSmartWalletVersions(chainId)).toEqual(Object.values(SMART_WALLET_VERSIONS[chainId]))
+    const chainIdValues = Object.values(SupportedChainIds)
+        .filter(value => typeof value === 'number');
+
+    for (const chainId of chainIdValues) {
+      expect(getAllSmartWalletVersions(chainId as SupportedChainIds)).toEqual(Object.values(SMART_WALLET_VERSIONS[chainId as SupportedChainIds]))
+    }
   })
 })
