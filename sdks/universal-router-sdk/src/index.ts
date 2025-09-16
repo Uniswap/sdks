c247f1e8467105108d8f751296e587fa5849f981
abs3ntdev <abs3ntdev@users.noreply.github.com>
2025-09-16 14:13:37 -0700

    export

--- sdks/universal-router-sdk/src/index.ts
+++ sdks/universal-router-sdk/src/index.ts
@@ -1,16 +1,28 @@
-export { SwapRouter } from './swapRouter'
-export type { MigrateV3ToV4Options } from './swapRouter'
-export * from './entities'
-export * from './utils/routerTradeAdapter'
-export { RoutePlanner, CommandType, COMMAND_DEFINITION, Parser, Subparser } from './utils/routerCommands'
-export type { CommandDefinition, ParamType } from './utils/routerCommands'
+export { SwapRouter } from "./swapRouter";
+export type { MigrateV3ToV4Options } from "./swapRouter";
+export * from "./entities";
+export * from "./utils/routerTradeAdapter";
+export {
+  RoutePlanner,
+  CommandType,
+  COMMAND_DEFINITION,
+  Parser,
+  Subparser,
+} from "./utils/routerCommands";
+export type { CommandDefinition, ParamType } from "./utils/routerCommands";
 export {
   UNIVERSAL_ROUTER_CREATION_BLOCK,
   UNIVERSAL_ROUTER_ADDRESS,
   ROUTER_AS_RECIPIENT,
   WETH_ADDRESS,
   UniversalRouterVersion,
-} from './utils/constants'
-export { CommandParser, GenericCommandParser } from './utils/commandParser'
-export type { UniversalRouterCommand, UniversalRouterCall, Param, CommandsDefinition } from './utils/commandParser'
-export type { Permit2Permit } from './utils/inputTokens'
+  RESOLVE_PERMIT2_ADDRESS,
+} from "./utils/constants";
+export { CommandParser, GenericCommandParser } from "./utils/commandParser";
+export type {
+  UniversalRouterCommand,
+  UniversalRouterCall,
+  Param,
+  CommandsDefinition,
+} from "./utils/commandParser";
+export type { Permit2Permit } from "./utils/inputTokens";
