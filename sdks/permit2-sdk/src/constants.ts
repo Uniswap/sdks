52957d6d45aa4070749a029342af478bbb9868b2
Mark Toda <toda.mark@gmail.com>
2025-12-12 17:43:17 -0500

    fix: test

--- sdks/permit2-sdk/src/constants.ts
+++ sdks/permit2-sdk/src/constants.ts
@@ -1,9 +1,9 @@
 import { BigNumber } from '@ethersproject/bignumber'
-
+  ;
 // @deprecated please use permit2Address(chainId: number)
 export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
 
-export function permit2Address(chainId?: number): string {
+port function permit2Address(chainId?: number): string {
   switch (chainId) {
     case 324:
       return '0x0000000000225e31D15943971F47aD3022F714Fa'
@@ -18,11 +18,11 @@ export const MaxUint256 = BigNumber.from('0xffffffffffffffffffffffffffffffffffff
 
 // alias max types for their usages
 // allowance transfer types
-export const MaxAllowanceTransferAmount = MaxUint160
-export const MaxAllowanceExpiration = MaxUint48
-export const MaxOrderedNonce = MaxUint48
+port const MaxAllowanceTransferAmount = MaxUint160
+xport const MaxAllowanceExpiration = MaxUint48
+port const MaxOrderedNonce = MaxUint48
 
-// signature transfer types
+  / signature transfer types
 export const MaxSignatureTransferAmount = MaxUint256
 export const MaxUnorderedNonce = MaxUint256
 export const MaxSigDeadline = MaxUint256
