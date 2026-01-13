9a69ef5f969ce9e4ec1c150fa10ba66d63f4daba
dianakocsis <diana.kocsis@uniswap.org>
2026-01-13 15:15:55 -0500

    add comment to trigger release

--- sdks/v4-sdk/src/entities/route.ts
+++ sdks/v4-sdk/src/entities/route.ts
@@ -38,6 +38,8 @@ export class Route<TInput extends Currency, TOutput extends Currency> {
     this.pathInput = getPathCurrency(input, pools[0])
     this.pathOutput = getPathCurrency(output, pools[pools.length - 1])
 
+    // If the input is native and the first pool is an eth-weth pool, that means we already wrapped the input to weth
+    // so we need to set the path input to be the wrapped input
     if (this.pathInput.isNative && pools[0].currency0.wrapped.equals(pools[0].currency1)) {
       this.pathInput = pools[0].currency1
     }
@@ -73,23 +75,23 @@ export class Route<TInput extends Currency, TOutput extends Currency> {
       ({ nextInput, price }, pool) => {
         return nextInput.equals(pool.currency0)
           ? {
-              nextInput: pool.currency1,
-              price: price.multiply(pool.currency0Price),
-            }
+            nextInput: pool.currency1,
+            price: price.multiply(pool.currency0Price),
+          }
           : {
-              nextInput: pool.currency0,
-              price: price.multiply(pool.currency1Price),
-            }
+            nextInput: pool.currency0,
+            price: price.multiply(pool.currency1Price),
+          }
       },
       this.pools[0].currency0.equals(this.pathInput)
         ? {
-            nextInput: this.pools[0].currency1,
-            price: this.pools[0].currency0Price,
-          }
+          nextInput: this.pools[0].currency1,
+          price: this.pools[0].currency0Price,
+        }
         : {
-            nextInput: this.pools[0].currency0,
-            price: this.pools[0].currency1Price,
-          }
+          nextInput: this.pools[0].currency0,
+          price: this.pools[0].currency1Price,
+        }
     ).price
 
     return (this._midPrice = new Price(this.input, this.output, price.denominator, price.numerator))
